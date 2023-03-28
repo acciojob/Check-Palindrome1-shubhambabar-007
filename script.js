1c11e1e831526da15404459311589043debf586d
shubhambabar-007 <104158403+shubhambabar-007@users.noreply.github.com>
2023-03-28 15:55:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // complete the given function
 
 function palindrome(str){
-	str=str.replaceAll(" ","")
+	str=str.replaceAll(" ","").toLowerCase()
 	var str2=str.split("").reverse().join("");
 	if(str==str2)
 		return true
